b34308f8a8e8e86568dcc2fa3a56e783869c869d
SHERIF AHMED <sherif@indexgroup.net>
2020-09-07 15:19:45 +0200

    init project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5f6ec8ab56f180741eab90d6969b025c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c0c604be-4de4-45f6-a2ca-f129418861cc'
+};
